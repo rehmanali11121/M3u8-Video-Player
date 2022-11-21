804226cf26d790213fa1af5c2f51383929fde74b
Rehman_ali <rehmanali11121@gmail.com>
2022-11-21 07:50:32 +0500

    update video committed

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -2,4 +2,6 @@
     <source src="https://7rgnwbg2npl8-hls-push.5centscdn.com/PROMO%202%20ICC%20MENS%20T20%20WC%202022_OCT13_WEB.mp4/playlist.m3u8" type="application/x-mpegURL">
 </video>
 
-<?...https://7rgnwbg2npl8-hls-push.5centscdn.com/PROMO%202%20ICC%20MENS%20T20%20WC%202022_OCT13_WEB.mp4/playlist.m3u8?>
+<?
+https://7rgnwbg2npl8-hls-push.5centscdn.com/PROMO%202%20ICC%20MENS%20T20%20WC%202022_OCT13_WEB.mp4/playlist.m3u8
+?>
