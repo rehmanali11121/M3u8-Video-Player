0d40d3305e83718a3a647f07ea14439b5ccbba18
Rehman_ali <rehmanali11121@gmail.com>
2022-11-21 07:30:02 +0500

    edit video size to fluid

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,4 +1,3 @@
-<video controls preload="auto" data-setup="{}" class="video-js">
+<video controls preload="auto" data-setup='{"fluid": true}' class="video-js">
     <source src="https://stream1.aryzap.com/c8d6ae82971bfff2c496386779e210cd/6371f56a/v1/0182f8a158d80b8ed5aa6eddaece/0182f8b509730b8ed5aa6eddaed1/main.m3u8" type="application/x-mpegURL">
 </video>
-//https://new.ptvcrickethd.com/pak/media.stream/playlist.m3u8
