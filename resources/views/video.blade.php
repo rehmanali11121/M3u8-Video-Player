5ba7fd938edbe013a30c81c6abb35395371e3460
Rehman_ali <rehmanali11121@gmail.com>
2022-11-21 07:25:40 +0500

    First commit

---
+++ resources/views/video.blade.php
@@ -0,0 +1,4 @@
+<video controls preload="auto" data-setup="{}" class="video-js">
+    <source src="https://stream1.aryzap.com/c8d6ae82971bfff2c496386779e210cd/6371f56a/v1/0182f8a158d80b8ed5aa6eddaece/0182f8b509730b8ed5aa6eddaed1/main.m3u8" type="application/x-mpegURL">
+</video>
+//https://new.ptvcrickethd.com/pak/media.stream/playlist.m3u8
